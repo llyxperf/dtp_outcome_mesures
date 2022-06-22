86932029f2fa891559f13eb071f20ffa48ae4253
llyxperf <1579494058@qq.com>
2022-06-22 06:25:37 -0700

    APIcase update

--- data_process.py
+++ data_process.py
@@ -2,6 +2,7 @@
 This module is used for processing the DTP block data genered by the sender and reciever
 """
 
+import sys
 import os, platform, json
 import sys
 import time
@@ -9,8 +10,40 @@ import re
 import argparse
 from typing import DefaultDict
 from tqdm import tqdm
+import linecache
+#graph
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+
+
+#global
+MAX_BLOCK_SIZE = 1000000 # 1Mbytes
+tmp_dir_path='.'
+CLIENT_LOG_PATTERN = re.compile(r'connection closed\nrecv=(-?\d+) sent=(-?\d+) lost=(-?\d+) rtt=(?:(?:(\d|.+)ms)|(?:(-1))) cwnd=(-?\d+), total_bytes=(-?\d+), complete_bytes=(-?\d+), good_bytes=(-?\d+), total_time=(-?\d+)')
+CLIENT_STAT_INDEXES = ["c_recv", "c_sent", "c_lost", "c_rtt(ms)", "c_cwnd", "c_total_bytes", "c_complete_bytes", "c_good_bytes", "c_total_time(us)", "qoe", "retry_times"]
+CLIENT_BLOCKS_INDEXES = ["BlockID", "bct", "BlockSize", "Priority", "Deadline"]
 
 """Process the result"""
+#graph
+def drawGraph(axisX,axisYList,labelNameList,interval=0.2):
+    '''
+    usage:To draw a graph.
+    axisX:list,usually time
+    axisYList:list[],each element is a axisY.
+    '''
+    
+    plt.clf()
+
+    plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%.1f %%'))
+    plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%.1fs'))
+    plt.xlabel('time')
+    plt.ylabel('rate')
+    plt.title('DTP live show')
+    for i in range(len(axisYList)):
+        plt.plot(axisX,axisYList[i],label=labelNameList[i])
+
+    plt.legend()
+    plt.pause(interval)
 
 #client.log
 
@@ -101,63 +134,124 @@ def parse_client_result(config, cc_algo, logs_path, store_path):
             f.write('\n')
 
 
-def calculate_statiscs(logPath='./client.log',draw=False):
+def parse_client_log(dir_path):
+    '''
+    Parse client.log and get two dicts of information.
+    `client_blocks_dict` stores information in client.log about block's stream_id, bct, deadline and priority
+    `client_stat_dict` stores statistics offered in client.log. Some important information is like goodbytes and total running time(total time)
+    '''
+    # collect client blocks information
+    client_blocks_dict = {}
+    for index in CLIENT_BLOCKS_INDEXES:
+        client_blocks_dict[index] = []
+    # collect client stats
+    client_stat_dict = {}
+    for index in CLIENT_STAT_INDEXES:
+        client_stat_dict[index] = []
 
-"""
-Parse the content tn the Client.log,return a dict contains the statistics of the outcome mesures.
+    with open(os.path.join(dir_path, "client.log")) as client:
+        client_lines = client.readlines()
 
-keys:
-'TotalBlockNum':Number of blocks planning to send by the server.
-'BlockSentNum':Number of blocks sent by the server.
-'BlockRecvNum':Number of blocks recieved by the client
-'BlockInTime':Number of blocks arrived before the ddl.
-'HighpriBlockInTime':the number of high priority blocks arrived before the ddl.
-'AvrTime':Average BCT(ms)
+        for line in client_lines[4:-1]:
+            if len(line) > 1:
+               client_line_list = line.split()
+               if len(client_line_list) != len(CLIENT_BLOCKS_INDEXES):
+                   print("A client block log line has error format in : %s. This happens sometime." % dir_path)
+                   continue
+               for i in range(len(client_line_list)):
+                   client_blocks_dict[CLIENT_BLOCKS_INDEXES[i]].append(client_line_list[i])
 
-if parameter draw set to True,Draw a picture with y value as block-in-time rate and x value as time.
-"""
+        # try to parse the last line of client log
+        try:
+            lastLine=client_lines[-1][0:-1].split(',')
+            print(lastLine)
+            if len(lastLine)!=9:
+                raise ValueError("client re match returns None in : %s" % dir_path, client_lines[-1])
+
+            client_stat_dict["c_recv"].append(float(lastLine[0]))
+            client_stat_dict["c_sent"].append(float(lastLine[1]))
+            client_stat_dict["c_lost"].append(float(lastLine[2]))
+            client_stat_dict["c_rtt(ms)"].append(float(lastLine[3]))
 
 
+            client_stat_dict["c_cwnd"].append(float(lastLine[4]))
+            client_stat_dict["c_total_bytes"].append(float(lastLine[5]))
+            client_stat_dict["c_complete_bytes"].append(float(lastLine[6]))
+            client_stat_dict["c_good_bytes"].append(float(lastLine[7]))
+            client_stat_dict["c_total_time(us)"].append(float(lastLine[8]))
+
+            # invalid stat
+            client_stat_dict["qoe"].append(-1)
+            client_stat_dict["retry_times"].append(-1)
+
+            return client_blocks_dict, client_stat_dict
+
+        except Exception:
+            print(dir_path)
+            print(client_lines[-1])
+            raise ValueError("Could not parse client's last line")
+
+
+
+
+
+def calculate_statiscs(servErrPath,blockConfPath,logPath='./client.log',draw=False):
+    """
+    Parse the content tn the Client.log,return a dict contains the statistics of the outcome mesures.
+    keys:
+    'TotalBlockNum':Number of blocks planning to send by the server.
+    'BlockSentNum':Number of blocks sent by the server.
+    'BlockRecvNum':Number of blocks recieved by the client
+    'BlockInTime':Number of blocks arrived before the ddl.
+    'HighpriBlockInTime':the number of high priority blocks arrived before the ddl.
+    'AvrTime':Average BCT(ms)
+
+    if parameter draw set to True,Draw a picture with y value as block-in-time rate and x value as time.
+    """
     logStart='BlockID  bct  BlockSize  Priority  Deadline\n'
     
+    # collect client blocks information
+    client_blocks_dict = {}
+    for index in CLIENT_BLOCKS_INDEXES:
+        client_blocks_dict[index] = []
+    # collect client stats
+    client_stat_dict = {}
+    for index in CLIENT_STAT_INDEXES:
+        client_stat_dict[index] = []
+        
     if os.path.isfile(logPath)==False:
         print("Error calculate_statiscs:clint.log not found")
         return
     flag=True
     
     lineNum=4 #offset
-
+    resDict={}
    #读头
     line=linecache.getline(logPath,lineNum)
     if line==logStart:
-       lineNum+=1
+        lineNum+=1
     else:
-       print("Error calculate_statiscs:incomplete clitnt.log")
-       return
+        print("Error calculate_statiscs:incomplete clitnt.log")
+        return
 
     #analysing
     
     endFlagstr='connection closed\n'
     sendBlockNum=len(open(blockConfPath,'r').readlines()) #发送块数
+
     totalBct=0      #总发送时间
-    highPriNum=0	#高优先级块数
-    highPri_late=0	#超时到达高优先级块数
-    total_late =0 	#超时块数
+    highPriNum=0  #高优先级块数
+    highPri_late=0  #超时到达高优先级块数
+    total_late =0   #超时块数
     recvBlockNum=0        #总接收块
     flag=True
 
     #drawing
-
     timeX=[]
     high_rateY=[]
     rateY=[]
     avrTimeY=[]
     curTime=0
-
-    interval=0.2 #sec
-
-    #plt.ion()
-
 
     while flag:
         changeFlag=False#output when new lines arrive
@@ -188,6 +282,15 @@ if parameter draw set to True,Draw a picture with y value as block-in-time rate 
                     total_late+=1
                     if p[3]==1:
                         highPri_late +=1
+                        
+                if draw == True:
+                    curTime+=bct
+                    timeX.append(curTime)
+                    highBlockRate=round(100*(highPriNum-highPri_late)/highPriNum,2)
+                    high_rateY.append(highBlockRate)
+                    blockRate=round((recvBlockNum-total_late)/recvBlockNum*100,2)
+                    rateY.append(blockRate)
+                    
         #continue
         if changeFlag==True and draw ==True:
             #print('已接收{}块，平均完成时间为{}s,高优先级块按时完成率为{}%,所有块按时完成率为{}%'.format(recvBlockNum,round(totalBct/recvBlockNum,2),100*(highPriNum-highPri_late)/highPriNum,(recvBlockNum-total_late)/sendBlockNum*100))
@@ -198,24 +301,14 @@ if parameter draw set to True,Draw a picture with y value as block-in-time rate 
             timeX.append(curTime)
             high_rateY.append(highBlockRate)
             rateY.append(blockRate)
+            #draw
+            drawGraph(timeX,[rateY,high_rateY],['Total','HighPriority'])
             
-            plt.clf()
-           # print(curTime)
-            #print(high_rateY)
-            plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%.1f %%'))
-            plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%.1fs'))
-            plt.xlabel('time')
-            plt.ylabel('rate')
-            plt.title('DTP live show')
-            plt.plot(timeX,high_rateY,label='HighPriority')
-            plt.plot(timeX, rateY, label='Total')
-            plt.legend()
-            plt.pause(interval)
             print('已接收{}块，平均完成时间为{}ms,高优先级块按时完成率为{}%'.format(recvBlockNum,avrTime,highBlockRate))
        #blocks num lines of blocks
 
 
-    servErrPath='./log/server_err.log'
+    
     #logs_Path=resultPath+'/logs'
 
     blockDropNum=0
@@ -226,6 +319,72 @@ if parameter draw set to True,Draw a picture with y value as block-in-time rate 
     blockSentNum=sendBlockNum-blockDropNum
     
     
-    resDict={'TotalBlockNum':sendBlockNum,'BlockSentNum':blockSentNum,'BlockRecvNum':recvBlockNum,'BlockInTime':recvBlockNum-total_late,'HighpriBlockInTime':highPriNum-highPri_late,'AvrTime':round(totalBct/recvBlockNum,2))}
-    
-    return resDict
+    resDict={'TotalBlockNum':sendBlockNum,'BlockSentNum':blockSentNum,'BlockRecvNum':recvBlockNum,'BlockInTime':recvBlockNum-total_late,'HighpriBlockInTime':highPriNum-highPri_late,'AvrTime':round(totalBct/recvBlockNum,2)}
+
+    return resDict,timeX,rateY,high_rateY
+
+#def writeToLog(form,logPath,args:list):
+    """
+    for the client.write the log
+    form:
+    1:
+    2:
+    3:
+    args:
+    as for 1:
+    as for 2:
+    as for 3:
+    """
+#    with open(os.path.join(tmp_dir_path, 'aitrans_block.txt'), 'a') as f:
+#        f.write('%f    %d    %d    %d\n' % (block_gap * i, 2147483647, block_size, 0))
+
+
+def generate_block_file(config: dict):
+    '''
+    generate block file with the config
+    '''
+
+    block_size = min(int(config['block_size']), MAX_BLOCK_SIZE)
+    if block_size <= 0:
+        block_size = MAX_DATAGRAM_SIZE
+    block_num = int(config['block_num'])
+    block_gap = 0.001
+    if config.get("block_gap"):
+       block_gap = int(config["block_gap"])
+    with open(os.path.join(tmp_dir_path, 'aitrans_block.txt'), 'w') as f:
+        for i in range(block_num):
+            f.write('%f    %d    %d    %d\n' % (block_gap * i, 2147483647, block_size, 0))
+
+def generate_consistent_network_file(config):
+    '''
+    generate consistent network trace
+    example: 0, 1, 0.00001, 0.1 # @0 second, 1Mbps, loss rate=0.00001, 100ms latency
+    '''
+    with open(os.path.join(tmp_dir_path, 'traces.txt'), 'w') as f:
+        f.write('%d, %f, %f, %f' % (0, float(config['bw']), float(config['loss']), float(config['rtt']) / 2 / 1000))
+
+
+def parse_config(filepath: str):
+    '''
+    Parse the given config file and return the json obj
+    '''
+    with open(filepath, 'r') as f:
+        return json.load(f)[0]
+
+
+def generate_consistent_network_file(config):
+    '''
+    generate consistent network trace
+    example: 0, 1, 0.00001, 0.1 # @0 second, 1Mbps, loss rate=0.00001, 100ms latency
+    '''
+    with open(os.path.join(tmp_dir_path, 'traces.txt'), 'w') as f:
+        f.write('%d, %f, %f, %f' % (0, float(config['bw']), float(config['loss']), float(config['rtt']) / 2 / 1000))
+
+def generate_redundancy_code_file(config):
+    with open(os.path.join(tmp_dir_path, 'redundant.cpp'), 'w') as f:
+        f.write( \
+            '''
+            #include "solution.hxx"
+            float global_redundancy = {0};
+            '''.format(max(0.0, float(config['redundancy']))) \
+        )
